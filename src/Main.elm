6c0687639723700d1e36c4f3bae1b8c54a831c8b
Max P <maxfarseer@gmail.com>
2019-04-27 09:38:59 +0300

    add (Login) add (LeaderBoard) add (Main) new markup with 2 pages

--- src/Main.elm
+++ src/Main.elm
@@ -4,20 +4,34 @@ import Browser
 import Html exposing (Html)
 import Html.Attributes as Attr
 import Html.Events as Events
+import LeaderBoard
+import Login
 
 
 type alias Model =
-    {}
-
-
-type Msg
-    = Test
-    | OneMore
+    { page : Page
+    , leaderBoard : LeaderBoard.Model
+    , login : Login.Model
+    }
 
 
 initModel : Model
 initModel =
-    {}
+    { page = LeaderBoardPage
+    , leaderBoard = LeaderBoard.initModel
+    , login = Login.initModel
+    }
+
+
+type Page
+    = LeaderBoardPage
+    | LoginPage
+
+
+type Msg
+    = ChangePage Page
+    | LeaderBoardMsg LeaderBoard.Msg
+    | LoginMsg Login.Msg
 
 
 init : () -> ( Model, Cmd Msg )
@@ -28,8 +42,14 @@ init _ =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            ( model, Cmd.none )
+        ChangePage page ->
+            ( { model | page = page }, Cmd.none )
+
+        LeaderBoardMsg lbMsg ->
+            ( { model | leaderBoard = LeaderBoard.update lbMsg model.leaderBoard }, Cmd.none )
+
+        LoginMsg loginMsg ->
+            ( { model | login = Login.update loginMsg model.login }, Cmd.none )
 
 
 main : Program () Model Msg
@@ -44,7 +64,31 @@ main =
 
 view : Model -> Html Msg
 view model =
-    Html.div [ Attr.class "leaderboard" ]
-        [ Html.h1 []
-            [ Html.text "Racing leader board" ]
+    let
+        page =
+            case model.page of
+                LeaderBoardPage ->
+                    Html.map LeaderBoardMsg
+                        (LeaderBoard.view model.leaderBoard)
+
+                LoginPage ->
+                    Html.map LoginMsg
+                        (Login.view model.login)
+    in
+    Html.div []
+        [ Html.div []
+            [ Html.a
+                [ Attr.href "#"
+                , Events.onClick (ChangePage LeaderBoardPage)
+                ]
+                [ Html.text "LeaderBoard" ]
+            , Html.span [] [ Html.text " | " ]
+            , Html.a
+                [ Attr.href "#"
+                , Events.onClick (ChangePage LoginPage)
+                ]
+                [ Html.text "Login" ]
+            ]
+        , Html.hr [] []
+        , page
         ]
